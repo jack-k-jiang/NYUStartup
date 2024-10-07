8bc29237f11b3ed30bf860c6949caa4d97609c6c
Tristan Shi <tristan_shi@outlook.com>
2024-10-07 11:13:27 -0400

    attempted stack navigation (still buggy)

--- unimarket-dev/app/index.tsx
+++ unimarket-dev/app/index.tsx
@@ -3,6 +3,20 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@/screens/homescreen';
+import MarketPlaceScreen from '@/screens/marketplacescreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const HomeStack = createNativeStackNavigator()
+
+function HomeStackScreen() {
+    return (
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="Home" component={HomeScreen} />
+        <HomeStack.Screen name="MarketplaceScreen" component={MarketPlaceScreen} />
+      </HomeStack.Navigator>
+    );
+  }
 
 
 function SettingsScreen() {
@@ -18,7 +32,7 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeStackScreen} options={{tabBarBadge: 6}}/>
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
   );
---
+++ unimarket-dev/screens/marketplacescreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+//edit homescreen here
+
+function MarketPlaceScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{textAlign: 'center'}
+    }>
+            THIS {'\n'}
+            bob*6
+            </Text>
+      </View>
+    );
+  }
+export default MarketPlaceScreen
